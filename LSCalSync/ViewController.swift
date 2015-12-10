0d25bdec30dce47b4a7c7f33eb8f2dd0a0026956
block7 <block7@b339-computer-lab-86.local>
2015-12-10 13:40:07 -0500

    stuff

--- LSCalSync/ViewController.swift
+++ LSCalSync/ViewController.swift
@@ -7,19 +7,33 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        getAPI()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func getAPI() {
+        Alamofire.request(.GET, "https://mylsapp.com/api/v1/calendar_days.json").validate().responseJSON { response in
+            switch response.result {
+            case .Success:
+                if let value = response.result.value {
+                    let json = JSON(value)
+                    print("JSON: \(json)")
+                }
+            case .Failure(let error):
+                print(error)
+            }
+        }
+    }
 
 }
-
